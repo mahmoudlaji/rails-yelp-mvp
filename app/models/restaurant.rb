c2ebb31fbd278af889576e2a4c8bc242a45b9544
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-11-15 12:47:33 +0100

    interface restaurant is done with all actions et View Router Model Validates with Categorys

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
- 
+CATEGORYS =  ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :adresse, presence: true
+  validates :category, presence: true, inclusion: {in:CATEGORYS}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'reviews/new'
-  resources :restaurants do
-    collection do
-      get :best
-    end
-    resources :reviews, only: [:new, :create]
-  end
-  resources :reviews, only: [:destroy]
+  resources :restaurants
+  # do
+  #   collection do
+  #     get :best
+  #   end
+    # resources :reviews, only: [:new, :create]
+  # end
+  # resources :reviews, only: [:destroy]
 end
