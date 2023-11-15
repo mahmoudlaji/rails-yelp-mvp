d0ba9675b9cd2318aa4c231a658402e6f57d4f2e
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-11-15 11:49:19 +0100

    restaurants controller et view et router is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-   resources :restaurants
+  get 'reviews/new'
+  resources :restaurants do
+    collection do
+      get :best
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
